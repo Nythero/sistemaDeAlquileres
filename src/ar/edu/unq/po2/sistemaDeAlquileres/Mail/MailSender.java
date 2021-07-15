63cc6c0e63b0660c93ec55819180982d13451b3f
Nythero <agustin-ag@live.com>
2021-07-15 17:28:57 -0300

    Comentario del mailSender

--- src/ar/edu/unq/po2/sistemaDeAlquileres/Mail/MailSender.java
+++ src/ar/edu/unq/po2/sistemaDeAlquileres/Mail/MailSender.java
@@ -10,6 +10,11 @@ public class MailSender {
 		servidor = servidorNuevo;
 	}
 	
+	/**
+	 * Manda un mail notificando que la reserva fue cancelada al duenho del inmueble y a la persona que reservó el inmueble
+	 * 
+	 * @param reserva
+	 */
 	public static void mandarMailDeCancelacion(Reserva reserva) {
 		MailSender.mandarMail(reserva.getDuenho().getDireccionDeEmail(), 
 							  "soporte@sistemaAlquiler.com.ar", 
@@ -21,6 +26,11 @@ public class MailSender {
 				  "La reserva fue cancelada");
 	}
 
+	/**
+	 * Manda un mail notificando que la reserva fue aceptada al duenho del inmueble y a la persona que reservó el inmueble
+	 * 
+	 * @param reserva
+	 */
 	public static void mandarMailDeConfirmacion(Reserva reserva) {
 		MailSender.mandarMail(reserva.getDuenho().getDireccionDeEmail(), 
 				  "soporte@sistemaAlquiler.com.ar", 
@@ -32,6 +42,13 @@ public class MailSender {
 				  "La reserva fue confirmada");
 	}
 	
+	/**
+	 * Manda un mail al destinatario, de parte del remitente con el asunto y el mensaje dados
+	 * @param emailDestinatario
+	 * @param emailRemitente
+	 * @param asunto
+	 * @param message
+	 */
 	public static void mandarMail(String emailDestinatario, String emailRemitente, String asunto, String message) {
 		Mail mail = new Mail(emailDestinatario, emailRemitente, asunto, message);
 		servidor.enviar(mail);
